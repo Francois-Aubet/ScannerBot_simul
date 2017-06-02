1dfdcf2cc3cceadd43182c3a4fb77db090e122ac
Francois Aubet <francois.aubet@gmail.com>
2017-06-02 19:18:35 +0200

    first after a too long time

---
+++ scannerbot_gazebo-build/CMakeFiles/controller_node.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/franz/catkin_ws/src/scannerbot_gazebo/src/controller.cpp" "/home/franz/catkin_ws/src/scannerbot_gazebo-build/CMakeFiles/controller_node.dir/src/controller.cpp.o"
+  "/home/franz/catkin_ws/src/scannerbot_gazebo/src/main.cpp" "/home/franz/catkin_ws/src/scannerbot_gazebo-build/CMakeFiles/controller_node.dir/src/main.cpp.o"
+  "/home/franz/catkin_ws/src/scannerbot_gazebo/src/neuralPosition.cpp" "/home/franz/catkin_ws/src/scannerbot_gazebo-build/CMakeFiles/controller_node.dir/src/neuralPosition.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_PACKAGE_NAME=\"scannerbot_gazebo\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/franz/catkin_ws/src/scannerbot_gazebo/include"
+  "/opt/ros/kinetic/include"
+  "/opt/ros/kinetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ scannerbot_gazebo-build/CMakeFiles/controller_node.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/controller_node.dir/src/main.cpp.o"
+  "CMakeFiles/controller_node.dir/src/controller.cpp.o"
+  "CMakeFiles/controller_node.dir/src/neuralPosition.cpp.o"
+  "devel/lib/scannerbot_gazebo/controller_node.pdb"
+  "devel/lib/scannerbot_gazebo/controller_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/controller_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
